6c0f0d417c582886c20e4fe4c4bc51e51bc41a29
tyk <527273931@qq.com>
2021-01-02 23:04:43 +0800

    例子：模板类和模板函数

--- data-structure/main.cpp
+++ data-structure/main.cpp
@@ -1,18 +1,40 @@
 #include <iostream>
 using namespace std;
 
-template<T>
-void Add(T a, T b)
+template <typename T>
+void Swap(T &num1, T &num2)
 {
-    T tmp = a;
-    a = b;
-    tmp = b;
+    T tmp = num1;
+    num1 = num2;
+    num2 = tmp;
 }
 
+
+template <typename T>
+class TempClass
+{
+public:
+   T Process(T v)
+   {
+       return v * v;
+   }
+};
 
 int main()
 {
-    Add(3, 4);
-    cout << "Hello World";
+    int a = 3;
+    int b = 4;
+
+    Swap<int>(a, b);
+    cout << "a = " << a << " " << "b = " << b << endl;
+
+    TempClass<int> tempA;
+
+    cout << tempA.Process(4) << endl;
+
     return 0;
 }
+
+
+
+
